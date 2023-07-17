5e59f7357416aaef77ee3b0783f5f70f6d3a41ce
Tim Maupin <timmaupin@live.com>
2023-07-17 15:53:35 -0500

    add in baseline code for retrieve

--- App.js
+++ App.js
@@ -1,7 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Linking, StyleSheet, Text, View } from 'react-native';
+import * as Contacts from 'expo-contacts';
+import { useEffect, useState } from 'react';
+
+const call = (contact) => {
+  let phoneNumber = contact.phoneNumber[0].number;
+  const link = `tel:${phoneNumber}`
+
+  Linking.canOpenURL(link)
+    .then(supported => Linking.openURL(link))
+    .catch(error => console.error(error));
+}
+
+const Item = ({ item }) => (
+  <View style={styles.item}>
+    <Button
+      onPress={() => call(item)}
+      title={item.name}
+      />
+  </View>
+);
 
 export default function App() {
+  const [contacts, setContacts] = useState([])
+
+  useEffect(() => {
+    const getContacts = async() => {
+      const { status } = await Contacts.requestPermissionAsync();
+      if (status === 'granted'){
+        const { data } = await Contacts.getContactsAsync();
+          console.log('My data is here', data[0].name);
+          setContacts(data)
+      }
+    }
+    getContacts();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
